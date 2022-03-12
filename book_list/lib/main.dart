7314d3cee78f85d55f9844619de2d686ad149663
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-03-12 18:44:28 +0900

    fix

---
+++ book_list/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'BookListSample',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("本一覧"),
+      ),
+      body: const Center(
+        child: Text(''),
+      ),
+    );
+  }
+}
--- book_list/test/widget_test.dart
+++ book_list/test/widget_test.dart
@@ -8,13 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ig_ui_app/main.dart';
-import 'package:ig_ui_app/pages/root_app.dart';
+import 'package:book_list/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(RootApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
