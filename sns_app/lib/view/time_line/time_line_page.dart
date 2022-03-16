90edd38d44024812d4e255beb4c7808d0d7614d9
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-03-16 23:30:17 +0900

    TimeLinePageのUI作成

---
+++ sns_app/lib/view/time_line/time_line_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:sns_app/model/account.dart';
+import 'package:sns_app/model/post.dart';
+
+class TimeLinePage extends StatefulWidget {
+  @override
+  State<TimeLinePage> createState() => _TimeLinePageState();
+}
+
+class _TimeLinePageState extends State<TimeLinePage> {
+  Account myAccount = Account(
+    id: '1',
+    name: 'Flutter User',
+    userId: 'flutter id',
+    imagePath:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeAx_d6XyicREzk1ykPoT1-vD6yKH9oTaO0w&usqp=CAU',
+    createdTime: DateTime.now(),
+    updatedTime: DateTime.now(),
+  );
+
+  List<Post> postList = [
+    Post(
+      id: '1',
+      content: '初めまして',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    ),
+    Post(
+      id: '2',
+      content: '初めまして2回',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'タイムライン',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Theme.of(context).canvasColor,
+        elevation: 2,
+      ),
+      body: ListView.builder(
+        itemCount: postList.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              border: index == 0
+                  ? Border(
+                      top: BorderSide(color: Colors.grey, width: 0),
+                      bottom: BorderSide(color: Colors.grey, width: 0),
+                    )
+                  : Border(
+                      bottom: BorderSide(color: Colors.grey, width: 0),
+                    ),
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 22,
+                  foregroundImage: NetworkImage(myAccount.imagePath),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  myAccount.name,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  myAccount.userId,
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              DateFormat('M/d/yy').format(
+                                postList[index].createdTime!,
+                              ),
+                            )
+                          ],
+                        ),
+                        Text(postList[index].content)
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
