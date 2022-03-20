ddb84df5f648068017f32134167908571694cd9a
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-03-20 19:52:30 +0900

    CreateAccountPageのUI作成

--- sns_app/lib/view/start_up/create_account_page.dart
+++ sns_app/lib/view/start_up/create_account_page.dart
@@ -8,8 +8,95 @@ class CreateAccuntPage extends StatefulWidget {
 }
 
 class _CreateAccuntPageState extends State<CreateAccuntPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController userIdController = TextEditingController();
+  TextEditingController selfIntroductionController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          '新規登録',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              CircleAvatar(
+                radius: 40,
+                child: Icon(Icons.add),
+              ),
+              Container(
+                width: 300,
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(hintText: '名前'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Container(
+                  width: 300,
+                  child: TextField(
+                    controller: userIdController,
+                    decoration: InputDecoration(hintText: 'ユーザーID'),
+                  ),
+                ),
+              ),
+              Container(
+                width: 300,
+                child: TextField(
+                  controller: selfIntroductionController,
+                  decoration: InputDecoration(hintText: '自己紹介'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Container(
+                  width: 300,
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(hintText: 'メールアドレス'),
+                  ),
+                ),
+              ),
+              Container(
+                width: 300,
+                child: TextField(
+                  controller: passController,
+                  decoration: InputDecoration(hintText: 'パスワード'),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (nameController.text.isNotEmpty &&
+                        userIdController.text.isNotEmpty &&
+                        selfIntroductionController.text.isNotEmpty &&
+                        emailController.text.isNotEmpty &&
+                        passController.text.isNotEmpty) {
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Text('アカウントを作成'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- sns_app/lib/view/start_up/login_page.dart
+++ sns_app/lib/view/start_up/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:sns_app/view/screen.dart';
+import 'package:sns_app/view/start_up/create_account_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -57,7 +58,10 @@ class _LoginPageState extends State<LoginPage> {
                       style: TextStyle(color: Colors.blue),
                       recognizer: TapGestureRecognizer()
                         ..onTap = () {
-                          print('アカウントを作成');
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => CreateAccuntPage()));
                         },
                     ),
                   ],
