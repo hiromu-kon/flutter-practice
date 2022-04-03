323d09c6fe9117df8a6218d4aff4181960b1fd16
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-04-03 21:40:57 +0900

    1時間毎の天気を表示

--- weather_app/lib/top_page.dart
+++ weather_app/lib/top_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:weather_app/weather.dart';
 
 class TopPage extends StatefulWidget {
@@ -11,6 +12,69 @@ class TopPage extends StatefulWidget {
 class _TopPageState extends State<TopPage> {
   Weather currentWeather =
       Weather(temp: 15, description: '晴れ', tempMax: 18, tempMin: 14);
+
+  List<Weather> hourlyWeather = [
+    Weather(
+        temp: 20,
+        description: '晴れ',
+        time: DateTime(2022, 10, 1, 10),
+        rainyPercent: 0),
+    Weather(
+        temp: 15,
+        description: '雨',
+        time: DateTime(2022, 10, 1, 11),
+        rainyPercent: 90),
+    Weather(
+        temp: 17,
+        description: '曇り',
+        time: DateTime(2022, 10, 1, 12),
+        rainyPercent: 10),
+    Weather(
+        temp: 19,
+        description: '晴れ',
+        time: DateTime(2022, 10, 1, 13),
+        rainyPercent: 0),
+    Weather(
+        temp: 20,
+        description: '晴れ',
+        time: DateTime(2022, 10, 1, 10),
+        rainyPercent: 0),
+    Weather(
+        temp: 15,
+        description: '雨',
+        time: DateTime(2022, 10, 1, 11),
+        rainyPercent: 90),
+    Weather(
+        temp: 17,
+        description: '曇り',
+        time: DateTime(2022, 10, 1, 12),
+        rainyPercent: 10),
+    Weather(
+        temp: 19,
+        description: '晴れ',
+        time: DateTime(2022, 10, 1, 13),
+        rainyPercent: 0),
+    Weather(
+        temp: 20,
+        description: '晴れ',
+        time: DateTime(2022, 10, 1, 10),
+        rainyPercent: 0),
+    Weather(
+        temp: 15,
+        description: '雨',
+        time: DateTime(2022, 10, 1, 11),
+        rainyPercent: 90),
+    Weather(
+        temp: 17,
+        description: '曇り',
+        time: DateTime(2022, 10, 1, 12),
+        rainyPercent: 10),
+    Weather(
+        temp: 19,
+        description: '晴れ',
+        time: DateTime(2022, 10, 1, 13),
+        rainyPercent: 0)
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +91,7 @@ class _TopPageState extends State<TopPage> {
             ),
             Text(currentWeather.description!),
             Text(
-              '${currentWeather.temp}',
+              '${currentWeather.temp}°',
               style: const TextStyle(fontSize: 80),
             ),
             Row(
@@ -39,7 +103,38 @@ class _TopPageState extends State<TopPage> {
                 ),
                 Text('最低:${currentWeather.tempMin}°'),
               ],
-            )
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Divider(
+              height: 0,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: hourlyWeather.map((weather) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0, vertical: 8.0),
+                    child: Column(
+                      children: [
+                        Text('${DateFormat('H').format(weather.time!)}時'),
+                        Text(
+                          '${weather.rainyPercent}%',
+                          style: TextStyle(color: Colors.lightBlueAccent),
+                        ),
+                        const Icon(Icons.wb_sunny_sharp),
+                        Text(
+                          '${weather.temp}°',
+                          style: TextStyle(fontSize: 18),
+                        )
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
           ],
         ),
       ),
