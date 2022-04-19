43fcd3dbb1211a99de1ed0c28fdb6b2c263a1ac2
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-04-19 23:00:28 +0900

    fix

---
+++ flutter_alert_app/lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
