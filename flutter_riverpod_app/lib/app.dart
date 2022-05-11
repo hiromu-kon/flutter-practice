7cd4cbfa6016d6976cd168e7b9493d3b748b2ab9
hiromu-kon <hiro.0405soccer@icloud.com>
2022-05-11 22:00:43 +0900

    fix

---
+++ flutter_riverpod_app/lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_app/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
