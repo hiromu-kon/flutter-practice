86ef2b0cd170e3c305ac12055ed73c3cc25d593d
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-05-18 22:58:23 +0900

    シェルファイルの作成

--- flutter_riverpod_app/test/widget_test.dart
+++ flutter_riverpod_app/test/widget_test.dart
@@ -5,15 +5,19 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// Flutter imports:
 import 'package:flutter/material.dart';
+
+// Package imports:
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_riverpod_app/main.dart';
+// Project imports:
+import 'package:flutter_riverpod_app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
