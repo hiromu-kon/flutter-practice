f9d72f8d65852697cb55b19d5892efb6769b4f83
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-05-28 22:20:36 +0900

    fix

---
+++ flutter_riverpod_practice/lib/app.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_practice/views/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: themeNavy,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        cardTheme: const CardTheme(
+          margin: EdgeInsets.zero,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 1,
+          toolbarHeight: 48,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+const primaryNavyColor = Color(0xFF0050aa);
+
+const MaterialColor themeNavy = MaterialColor(
+  0xFF0050aa,
+  <int, Color>{
+    50: primaryNavyColor,
+    100: primaryNavyColor,
+    200: primaryNavyColor,
+    300: primaryNavyColor,
+    400: primaryNavyColor,
+    500: primaryNavyColor,
+    600: primaryNavyColor,
+    700: primaryNavyColor,
+    800: primaryNavyColor,
+    900: primaryNavyColor,
+  },
+);
---
+++ flutter_riverpod_practice/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_practice/app.dart';
+
+void main() {
+  runApp(const App());
+}
