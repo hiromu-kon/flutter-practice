42b4b2bb95f269360fea7576c00fd2f5190acaf1
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-05-30 22:39:07 +0900

    fix

--- flutter_riverpod_practice/lib/views/home_page.dart
+++ flutter_riverpod_practice/lib/views/home_page.dart
@@ -1,13 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_practice/entities/account/account.dart';
+import 'package:flutter_riverpod_practice/entities/post/post.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  Account myAccount = Account(
+    id: '1',
+    name: 'Flutterラボ',
+    imagePath: 'https://img.icons8.com/color/344/flutter.png',
+    selfIntroduction: 'こんばんは',
+    userId: 'flutter_id',
+    createdTime: DateTime.now(),
+    updatedTime: DateTime.now(),
+  );
+
+  List<Post> postList = [
+    Post(
+      id: '1',
+      content: '初めまして',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    ),
+    Post(
+      id: '2',
+      content: '初めまして2回',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+      ),
+      body: ListView.builder(
+        itemCount: postList.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+                border: index == 0
+                    ? const Border(
+                        top: BorderSide(color: Colors.grey, width: 0),
+                        bottom: BorderSide(color: Colors.grey, width: 0))
+                    : const Border(
+                        bottom: BorderSide(color: Colors.grey, width: 0))),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 22,
+                  foregroundImage: NetworkImage(myAccount.imagePath.toString()),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(myAccount.name,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  '@${myAccount.userId}',
+                                  style: const TextStyle(color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                            Text(DateFormat('M/d/yy')
+                                .format(postList[index].createdTime))
+                          ],
+                        ),
+                        Text(postList[index].content)
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
