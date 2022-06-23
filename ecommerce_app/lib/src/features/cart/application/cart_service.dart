ad190a9d96531dc7c22b0100a759f28b6f786c2e
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-06-23 22:56:31 +0900

    fix

---
+++ ecommerce_app/lib/src/features/cart/application/cart_service.dart
@@ -0,0 +1,61 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
+import 'package:ecommerce_app/src/features/cart/domain/item.dart';
+import 'package:ecommerce_app/src/features/cart/domain/mutable_cart.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CartService {
+  CartService(this.ref);
+  final Ref ref;
+
+  /// fetch the cart from the local or remote repository
+  /// depending on the user auth state
+  Future<Cart> _fetchCart() {
+    final user = ref.read(authRepositoryProvider).currentUser;
+    if (user != null) {
+      return ref.read(remoteCartRepositoryProvider).fetchCart(user.uid);
+    } else {
+      return ref.read(localCartRepositoryProvider).fetchCart();
+    }
+  }
+
+  /// save the cart to the local or remote repository
+  /// depending on the user auth state
+  Future<void> _setCart(Cart cart) async {
+    final user = ref.read(authRepositoryProvider).currentUser;
+    if (user != null) {
+      await ref.read(remoteCartRepositoryProvider).setCart(user.uid, cart);
+    } else {
+      await ref.read(localCartRepositoryProvider).setCart(cart);
+    }
+  }
+
+  /// sets an item in the local or remote cart depending on the user auth state
+  Future<void> setItem(Item item) async {
+    final cart = await _fetchCart();
+    final updated = cart.setItem(item);
+    await _setCart(updated);
+  }
+
+  /// adds an item in the local or remote cart depending on the user auth state
+  Future<void> addItem(Item item) async {
+    final cart = await _fetchCart();
+    final updated = cart.addItem(item);
+    await _setCart(updated);
+  }
+
+  /// removes an item from the local or remote cart depending on the user auth
+  /// state
+  Future<void> removeItemById(ProductID productId) async {
+    final cart = await _fetchCart();
+    final updated = cart.removeItemById(productId);
+    await _setCart(updated);
+  }
+}
+
+final cartServiceProvider = Provider<CartService>((ref) {
+  return CartService(ref);
+});
